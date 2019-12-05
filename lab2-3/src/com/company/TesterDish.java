5b6392690422e0cc7ca974c7931ce38dfa6fe655
dmitry-ivkin <55837705+dmitry-ivkin@users.noreply.github.com>
2019-12-05 14:04:32 +0300

    Add files via upload

---
+++ lab2-3/src/com/company/TesterDish.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class TesterDish {
+    public static void main(String[] args) {
+        Tarelka t = new Tarelka("Керамическая", "Белый", "Большая");
+        System.out.println(t);
+    }
+}
